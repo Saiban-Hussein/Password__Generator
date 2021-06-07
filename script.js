7003005796d2fafbefe0afce19c5fac7766eb473
Saiban Hussein <shhussein@m.marywood.edu>
2021-06-07 00:40:23 -0500

    update js file the writePassword function

--- script.js
+++ script.js
@@ -23,3 +23,13 @@ var uppercaseArrayAndspecialLetters=["A", "B", "C", "D", "E", "F", "G", "H", "I"
 var uppercaseArrayAndnumbers=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",'1', '2', '3', '4', '5', '6', '7', '8', '9', ];
 
 var parsed = "";
+
+
+
+function writePassword() {
+  var password = generatePassword();
+  var element = document.getElementById("password");
+  element.textContent = parsed;
+}
+generateBtn.addEventListener("click", writePassword);
+
